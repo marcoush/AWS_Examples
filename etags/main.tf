05f1348b366d6e5a0760b898bf91618b58bc121c
marcoush <marcoush@users.noreply.github.com>
2026-05-03 15:34:21 +0000

    sync sync sync

--- etags/main.tf
+++ etags/main.tf
@@ -7,8 +7,18 @@ terraform {
     }
 }
 
+provider "aws" {
+    #coinfig options
+}
+
+resource "aws_s3_bucket" "default" {
+
+}
+
 resource "aws_s3_object" "object" {
-    bucket = resource.aws_s3_bucket.default
+    bucket = aws_s3_bucket.default.id
     key = "myfile.txt"
-    
+    source = "myfile.txt"
+
+    etag = filemd5("myfile.txt")
 }
